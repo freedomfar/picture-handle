3148c0f03ea987db4ea679da6fd9a03fd65925cb
hellofar <735786901@qq.com>
2018-12-02 12:12:24 +0800

    #10000 测试连接

--- src/main/java/com/personal/PictureHandleApplication.java
+++ src/main/java/com/personal/PictureHandleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PictureHandleApplication {
 
 	public static void main(String[] args) {
+		//hahhahahah
 		SpringApplication.run(PictureHandleApplication.class, args);
 	}
 }
