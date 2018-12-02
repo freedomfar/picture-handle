71f25e2c9c5b7869b58a06af3bcac443bfe9748d
hellofar <735786901@qq.com>
2018-12-02 11:56:32 +0800

    Initial commit

---
+++ src/main/java/com/personal/PictureHandleApplication.java
@@ -0,0 +1,12 @@
+package com.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureHandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureHandleApplication.class, args);
+	}
+}
