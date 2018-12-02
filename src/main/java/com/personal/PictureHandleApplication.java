f12021acf2dccca0a3fcfcaff05d4e6c2c885068
hellofar <735786901@qq.com>
2018-12-02 12:14:55 +0800

    #10000 测试连接

--- src/main/java/com/personal/PictureHandleApplication.java
+++ src/main/java/com/personal/PictureHandleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PictureHandleApplication {
 
 	public static void main(String[] args) {
-		//hahhahahah
+		//测试
 		SpringApplication.run(PictureHandleApplication.class, args);
 	}
 }
